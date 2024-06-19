c86bbd4e772e89eb53e9a24f94c87f0c6592f6af
Rakesh R <rakeshrake5398@gmail,com>
2024-06-19 16:12:04 +0530

    new one

---
+++ src/test/java/Testing_Academy/com/RA_01/LAB1.java
@@ -0,0 +1,14 @@
+package Testing_Academy.com.RA_01;
+
+import io.restassured.RestAssured;
+
+public class LAB1 {
+    public static void main(String[] args) {
+
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking")
+                .when().get().then().statusCode(200);
+    }
+}
